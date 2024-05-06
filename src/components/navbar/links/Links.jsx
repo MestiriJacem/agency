61c0ac97257dff640d770f3e58edc30b41455990
MestiriJacem <mestirijacemm@gmail.com>
2024-05-06 11:05:50 +0200

    links , Lading page

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,5 +1,23 @@
 const Links = () => {
-    
+    const Links =[
+        {
+            title:"Homepage",
+            path:"/",
+        },
+        {
+            title:"About",
+            path:"/about",
+        },
+        {
+            title:"Contact",
+            path:"/contact",
+        },
+        {
+            title:"Blog",
+            path:"/blog",
+        },
+        
+    ]
     return ( 
         <div>
             Links
