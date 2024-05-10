01a3f471ae34c5ea47a4043133dac445bae16c39
MestiriJacem <mestirijacemm@gmail.com>
2024-05-10 09:53:07 +0100

    responsive nav bar state

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,7 +1,9 @@
+"use client"
 
 import styles from'./links.module.css'
 import NavLink from "../navLink/navLink";
-const Links = () => {
+import { useState } from 'react';
+
     const links =[
 
         {
@@ -22,12 +24,44 @@ const Links = () => {
         },
         
     ];
+    const Links = () => {
+        const [open,setOpen] =useState(false)
+    //temporary
+    const session =true
+    const isAdmin =true
     return ( 
+        <div className={styles.container}>
         <div className={styles.links}>
         {links.map((link) => (
           <NavLink item={link} key={link.title} />
-        ))}
+        ))}{
+            session ? (
+                <>
+                {
+                    isAdmin && (
+                        <NavLink item={{title:"Admin",path:"/admin"}} />
+
+                    )
+                }
+                <button className={styles.logout}>Logout</button>
+                </>
+            ):(
+                <NavLink item={{title:"Login",path:"/login"}} />
+            )
+        }
         </div>
+        <button onClick={()=>setOpen((prev)=>!prev)}>Menu</button>
+        {
+            open && <div className={styles.mobileLinks}>
+                {links.map((link)=>(
+                    <NavLink item ={link} key ={link.title}/>
+
+                ))}
+
+            </div>
+        }
+        </div>
+       
      );
 }
  
