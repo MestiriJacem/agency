241c421560aca311e4c03691feb63e7e951aa34a
MestiriJacem <mestirijacemm@gmail.com>
2024-05-14 10:09:15 +0100

    Completing contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,33 @@
+import styles from './contact.module.css'
+import Image from 'next/image';
 const Contact  = () => {
     return (
-        <div>
-            contact 
+        <div className={styles.container}>
+            <div className={styles.imgContainer}>
+          <Image 
+          src="/contact.png"
+          alt=""
+          fill
+          className={styles.img}
+          
+          
+          />
+
+            </div>
+                <div className={styles.formContainer}>
+                    <form action="" className={styles.form}>
+                        <input type="text" placeholder='Name and surname' />
+                        <input type="text" placeholder='Email address' />
+                        <input type="text" placeholder='Phone number' />
+                        <textarea name="" id="" rows="10" cols="30">Message</textarea>
+                        <button>Send</button>
+                        
+                    </form>
+
+
+              
+
+            </div>
         </div>
       );
 }
