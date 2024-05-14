b1720a253b76d7ac93bdeafbf44d30b1477f21fb
MestiriJacem <mestirijacemm@gmail.com>
2024-05-14 08:22:54 +0100

    adding footer and finishing home page

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,14 @@
-const Footer =()=>{
-    return(
-        <div>
-        Footer
-        </div>
-    )
-}
+import styles from "./footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>lamadev</div>
+      <div className={styles.text}>
+        Lama creative thoughts agency © All rights reserved.
+      </div>
+    </div>
+  );
+};
+
 export default Footer;
